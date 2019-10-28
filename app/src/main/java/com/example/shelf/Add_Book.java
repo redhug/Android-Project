ff83dbacaacdede4cc87b0be6d8f5aac7879297c
Sushma Rani Reddy <reddysushma651@gmail.com>
2019-10-27 23:34:50 -0500

    Functionalities added and validations with error messages are created.

--- app/src/main/java/com/example/shelf/Add_Book.java
+++ app/src/main/java/com/example/shelf/Add_Book.java
@@ -10,55 +10,127 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.regex.Pattern;
+
 public class Add_Book extends AppCompatActivity {
+
+    EditText bookTitle;
+    EditText author;
+    EditText isbn;
+    EditText edition;
+    EditText bookCondition;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add__book);
+        bookTitle = findViewById(R.id.editText);
+        author = findViewById(R.id.editText1);
+        isbn = findViewById(R.id.editText2);
+        edition = findViewById(R.id.editText3);
+        bookCondition = findViewById(R.id.editText4);
+        submit = findViewById(R.id.button5);
     }
     public void buttonSave_onClick(View v) {
+        final String btitle=bookTitle.getText().toString();
+        final String bisbn=isbn.getText().toString();
+        final String bauthor=author.getText().toString();
+        final String bedition=edition.getText().toString();
+        final String bcondition=bookCondition.getText().toString();
 
-        EditText bookTitle = findViewById(R.id.editText);
-        String value1 = bookTitle.getText().toString();
-        TextView displayTV = findViewById(R.id.displayTV);
-        bookTitle.setText(value1);
 
-        if (value1.isEmpty()) {
-            displayTV.setText("please enter title of the book");
+
+        if(btitle.length()==0){
+            bookTitle.requestFocus();
+            bookTitle.setError("cannot be empty!!");
+        }
+        else if(!btitle.matches("[a-zA-Z ]+"))
+        {
+            bookTitle.requestFocus();
+            bookTitle.setError("ENTER ONLY ALPHABETICAL CHARACTER");
         }
 
-        EditText author = findViewById(R.id.editText1);
-        String value2 = author.getText().toString();
-        TextView displayTV1 = findViewById(R.id.displayTV);
-        author.setText(value2);
-        if (value2.isEmpty()) {
-            displayTV.setText("please enter author name");
+
+
+
+
+        if(bauthor.length()==0){
+            author.requestFocus();
+            author.setError("cannot be empty!!");
+        }
+        else if(!bauthor.matches("[a-zA-Z ]+"))
+        {
+            author.requestFocus();
+            author.setError("ENTER ONLY ALPHABETICAL CHARACTER");
         }
 
-        EditText isbn = findViewById(R.id.editText2);
-        String value3 = isbn.getText().toString();
-        int desiredValue = Integer.parseInt(value3);
-        TextView displayTV2 = findViewById(R.id.displayTV);
-        isbn.setText(value3);
 
-        if (value3.isEmpty()) {
-            displayTV.setText("please enter a number");
+        if(bisbn.length()==0){
+            isbn.requestFocus();
+            isbn.setError("cannot be empty!!");
         }
-        EditText edition = findViewById(R.id.editText3);
-        String value4 = edition.getText().toString();
-        TextView displayTV3 = findViewById(R.id.displayTV);
-        edition.setText(value4);
-        if (value4.isEmpty()) {
-            displayTV.setText("please enter edition number");
+        else if (!Pattern.matches("[0-20]+",bisbn)){
+            isbn.requestFocus();
+           // isbn.setError("isbn field should contain only numerical values");
         }
-        EditText bookCondition = findViewById(R.id.editText4);
-        String value5 = bookCondition.getText().toString();
-        TextView displayTV4 = findViewById(R.id.displayTV);
-        bookCondition.setText(value5);
-        if (value5.isEmpty()) {
-            displayTV.setText("please enter book condition");
+
+
+
+        if(bedition.length()==0){
+            edition.requestFocus();
+            edition.setError("cannot be empty!!");
+         // edition.setError("edition field should contain only numerical values");
+
         }
+        else if (!Pattern.matches("[0-20]+",bedition)){
+            edition.requestFocus();
+        }
+
+
+        if(bcondition.length()==0){
+            bookCondition.requestFocus();
+            bookCondition.setError("cannot be empty!!");
+        }
+        else if(!bcondition.matches("[a-zA-Z ]+"))
+        {
+            bookCondition.requestFocus();
+            bookCondition.setError("ENTER ONLY ALPHABETICAL CHARACTER");
+        }
+
+
+
+//        if(btitle.length()==0){
+//            bookTitle.requestFocus();
+//            bookTitle.setError("Name field cannot be empty!!");
+//        }
+//        else if(bauthor.length()==0){
+//            author.requestFocus();
+//            author.setError("Name field cannot be empty!!");
+//        }
+//        else if(bisbn.length()==0){
+//            isbn.requestFocus();
+//            isbn.setError(" field cannot be empty!!");
+//        }
+//        else if(bedition.length()==0){
+//            edition.requestFocus();
+//            edition.setError(" field cannot be empty!!");
+//        }
+//        else if(bcondition.length()==0){
+//            bookCondition.requestFocus();
+//            bookCondition.setError("Name field cannot be empty!!");
+//        }
+//
+
+        else {
+            try {
+                Intent toOtherIntent = new Intent(this, Add_Book.class);
+                startActivity(toOtherIntent);
+            } catch (Exception e) {
+            }
+        }
+
+
 
     }
         public void onClickFindBook(View v){
