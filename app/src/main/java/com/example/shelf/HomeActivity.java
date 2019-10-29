c6a1ddb9989144a6a7e980982261694e20e6c9e3
Pavan <s534625@nwmissouri.edu>
2019-10-28 19:38:10 -0500

    Resolved merge conflicts

---
+++ app/src/main/java/com/example/shelf/HomeActivity.java
@@ -0,0 +1,64 @@
+package com.example.shelf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.findBook:
+                Intent findBook = new Intent(this, Find_Book.class);
+                startActivityForResult(findBook, 1);
+                return(true);
+            case R.id.requests:
+                Intent requests = new Intent(this, Activity_Request.class);
+                startActivityForResult(requests, 1);
+                return(true);
+            case R.id.addBook:
+                Intent addBook = new Intent(this, Add_Book.class);
+                startActivityForResult(addBook, 1);
+                return(true);
+            case R.id.profile:
+                Intent profile = new Intent(this, ProfileActivity.class);
+                startActivityForResult(profile, 1);
+                return(true);
+        }
+        return(super.onOptionsItemSelected(item));
+    }
+
+    public void onClickBook(View v){
+        Intent bookInfo = new Intent(this, Book_Info_Activity.class);
+        startActivityForResult(bookInfo, 1);
+    }
+    public void onActivityResult(int requestCode,int resultCode,Intent tipInt) {
+        try {
+            if (requestCode == 1) {
+                if (resultCode == 0) {
+                    Log.d("Success", "got result");
+                }
+            }
+        }
+        catch (Exception e){
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/shelf/OwnerActivity.java
@@ -0,0 +1,68 @@
+package com.example.shelf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class OwnerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.home:
+                Intent home = new Intent(this, HomeActivity.class);
+                startActivityForResult(home, 1);
+                return(true);
+            case R.id.findBook:
+                Intent findBook = new Intent(this, Find_Book.class);
+                startActivityForResult(findBook, 1);
+                return(true);
+            case R.id.requests:
+                Intent requests = new Intent(this, Activity_Request.class);
+                startActivityForResult(requests, 1);
+                return(true);
+            case R.id.addBook:
+                Intent addBook = new Intent(this, Add_Book.class);
+                startActivityForResult(addBook, 1);
+                return(true);
+            case R.id.profile:
+                Intent profile = new Intent(this, ProfileActivity.class);
+                startActivityForResult(profile, 1);
+                return(true);
+        }
+        return(super.onOptionsItemSelected(item));
+    }
+
+    public void onClickOwner(View v){
+        Intent userIntent = new Intent(this, UserActivity.class);
+        startActivityForResult(userIntent, 1);
+    }
+    public void onActivityResult(int requestCode,int resultCode,Intent tipInt) {
+        try {
+            if (requestCode == 1) {
+                if (resultCode == 0) {
+                    Log.d("Success", "got result");
+                }
+            }
+        }
+        catch (Exception e){
+        }
+    }
+
+}
