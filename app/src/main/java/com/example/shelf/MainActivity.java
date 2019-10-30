e52586f89182c6a6d29029754cd38164bd0201f9
Anvesh <anveshrao1616@gmail.com>
2019-10-30 10:23:12 -0500

    Validate the login functionality connecting to the database
    Signed-off-by: Anvesh <anveshrao1616@gmail.com>

--- app/src/main/java/com/example/shelf/MainActivity.java
+++ app/src/main/java/com/example/shelf/MainActivity.java
@@ -2,39 +2,119 @@ package com.example.shelf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText emailID;
+    private EditText password;
+    public static String email;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailID =  findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        final Button login_button = findViewById(R.id.login);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Validating the log in data
+                boolean validationError = false;
+
+                StringBuilder validationErrorMessage = new StringBuilder("Please, insert ");
+                if (isEmpty(emailID)) {
+                    validationError = true;
+                    validationErrorMessage.append("an username");
+                }
+                if (isEmpty(password)) {
+                    if (validationError) {
+                        validationErrorMessage.append(" and ");
+                    }
+                    validationError = true;
+                    validationErrorMessage.append("a password");
+                }
+                validationErrorMessage.append(".");
+
+                if (validationError) {
+                    Toast.makeText(MainActivity.this, validationErrorMessage.toString(), Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                //Setting up a progress dialog
+                final ProgressDialog dlg = new ProgressDialog(MainActivity.this);
+                dlg.setTitle("Please, wait a moment.");
+                dlg.setMessage("Logging in...");
+                dlg.show();
+
+                ParseUser.logInInBackground(emailID.getText().toString(), password.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser parseUser, ParseException e) {
+                        if (parseUser != null) {
+                            dlg.dismiss();
+                            alertDisplayer("Sucessful Login","Welcome back " + emailID.getText().toString() + "!");
+
+                        } else {
+                            dlg.dismiss();
+                            ParseUser.logOut();
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        final Button signup_button = findViewById(R.id.Signup);
+        signup_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void loginClick(View view) {
-        EditText email=findViewById(R.id.email);
-        EditText pwd=findViewById(R.id.password);
-//        Intent signup=getIntent();
-//        String a=signup.getStringExtra("Email");
-//        String p=signup.getStringExtra("Password");
-//        if(a.equals(email.getText().toString())&&p.equals(pwd.getText().toString()))
-//        {
-            Intent homeIntent = new Intent(this, HomeActivity.class);
-            startActivityForResult(homeIntent, 1);
-//        }
-//        else {
-//            email.setError("Enter the valid Email ID");
-//            pwd.setError("Enter the password");
-//        }
-
+    private boolean isEmpty(EditText text) {
+        if (text.getText().toString().trim().length() > 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
-    public void onSignup(View view) {
-        Intent signUp = new Intent(this, SignupActivity.class);
-        startActivityForResult(signUp, 1);
+    private void alertDisplayer(String title,String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        email= emailID.getText().toString();
+                        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                        intent.putExtra("email", emailID.getText().toString());
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                });
+        AlertDialog ok = builder.create();
+        ok.show();
     }
 }
