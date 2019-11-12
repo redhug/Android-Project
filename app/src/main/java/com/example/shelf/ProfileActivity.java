4e98ad06fa54d768345642c2fa4364381076f2d4
Samanth <samanthrao226@gmail.com>
2019-11-11 18:45:49 -0600

    Minimal View Changes and removed static data
    Signed-off-by: Samanth <samanthrao226@gmail.com>

--- app/src/main/java/com/example/shelf/ProfileActivity.java
+++ app/src/main/java/com/example/shelf/ProfileActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.location.Address;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -50,6 +52,35 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.findBook:
+                Intent findBook = new Intent(this, Find_Book.class);
+                startActivityForResult(findBook, 1);
+                return(true);
+            case R.id.requests:
+                Intent requests = new Intent(this, Activity_Request.class);
+                startActivityForResult(requests, 1);
+                return(true);
+            case R.id.addBook:
+                Intent addBook = new Intent(this, Add_Book.class);
+                startActivityForResult(addBook, 1);
+                return(true);
+            case R.id.profile:
+                Intent profile = new Intent(this, ProfileActivity.class);
+                startActivityForResult(profile, 1);
+                return(true);
+        }
+        return(super.onOptionsItemSelected(item));
+    }
+    
     public void onLogout(View v){
         ParseUser.logOut();
         Toast.makeText(ProfileActivity.this,"Logging out", Toast.LENGTH_LONG).show();
